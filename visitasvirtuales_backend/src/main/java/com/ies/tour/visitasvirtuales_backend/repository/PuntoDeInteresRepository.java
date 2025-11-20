f8f14801d54e7162d35b0ef9343ab2ea26a73c17
Javier Vaquero <jvaquero03@gmail.com>
2025-11-20 13:17:02 +0100

    Add files via upload
    Vinculación con idCentro

--- visitasvirtuales_backend/src/main/java/com/ies/tour/visitasvirtuales_backend/repository/PuntoDeInteresRepository.java
+++ visitasvirtuales_backend/src/main/java/com/ies/tour/visitasvirtuales_backend/repository/PuntoDeInteresRepository.java
@@ -1,13 +1,16 @@
 package com.ies.tour.visitasvirtuales_backend.repository;
 
 import com.ies.tour.visitasvirtuales_backend.model.PuntoDeInteres;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PuntoDeInteresRepository extends JpaRepository<PuntoDeInteres, Integer> {
 
-    // Método para obtener todos los puntos de interes de una visita
-    // List<PuntoDeInteres> findByVisitaVirtual_IdVisitas(Integer idVisitas);
+    // Método para filtar los PDIs por centro
+    List<PuntoDeInteres> findByCentro_IdCentros(Integer idCentros);
 
 }
