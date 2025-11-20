e120885dfd27657d3881168dfe7433a5113f11fc
Javier Vaquero <jvaquero03@gmail.com>
2025-11-20 13:16:04 +0100

    Add files via upload
    Vinculación con idCentro

--- visitasvirtuales_backend/src/main/java/com/ies/tour/visitasvirtuales_backend/controller/PuntoDeInteresController.java
+++ visitasvirtuales_backend/src/main/java/com/ies/tour/visitasvirtuales_backend/controller/PuntoDeInteresController.java
@@ -1,6 +1,7 @@
 package com.ies.tour.visitasvirtuales_backend.controller;
 
 import com.ies.tour.visitasvirtuales_backend.dto.PuntoDeInteresDTO;
+import com.ies.tour.visitasvirtuales_backend.exception.ResourceNotFoundException;
 import com.ies.tour.visitasvirtuales_backend.model.PuntoDeInteres;
 import com.ies.tour.visitasvirtuales_backend.service.PuntoDeInteresService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,18 +40,23 @@ public class PuntoDeInteresController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    // RUTA 2: RUTA RELACIONAL (Obtener PDIs de una visita específica)
+    // EDITAR O ACTUALIZAR
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMINISTRADOR') or hasRole('PROFESOR')")
+    public ResponseEntity<PuntoDeInteres> actualizarPdi(
+            @PathVariable Integer id,
+            @Valid @RequestBody PuntoDeInteresDTO pdiDTO,
+            @RequestParam Integer idCentro) {
 
-    /*
-     * // GET /api/pdis/visita/{idVisita}
-     * 
-     * @GetMapping("/visita/{idVisita}")
-     * public ResponseEntity<List<PuntoDeInteres>> listarPorVisita(@PathVariable
-     * Integer idVisita) {
-     * List<PuntoDeInteres> pdis = puntoDeInteresService.findByVisitaId(idVisita);
-     * return ResponseEntity.ok(pdis);
-     * }
-     */
+        try {
+            PuntoDeInteres pdiActualizado = puntoDeInteresService.updatePdi(id, pdiDTO, idCentro);
+            // Devuelve 200 OK con el objeto actualizado
+            return ResponseEntity.ok(pdiActualizado);
+        } catch (ResourceNotFoundException e) {
+            // Captura la excepción 404 (si el PDI no existe)
+            return ResponseEntity.notFound().build();
+        }
+    }
 
     // DELETE /api/pdis/{id}
     @DeleteMapping("/{id}")
@@ -66,10 +72,23 @@ public class PuntoDeInteresController {
     @PostMapping("/infoPdi")
     @PreAuthorize("hasRole('ADMINISTRADOR') or hasRole('PROFESOR')")
     public ResponseEntity<PuntoDeInteres> crearPuntoDeInteres(
-            @Valid @RequestBody PuntoDeInteresDTO pdiDTO) {
+            @Valid @RequestBody PuntoDeInteresDTO pdiDTO,
+            @RequestParam Integer idCentro) {
         // 1. El servicio obtiene el usuario, mapea el DTO y guarda la entidad en la DB
-        PuntoDeInteres pdiGuardado = puntoDeInteresService.saveFromDto(pdiDTO);
+        PuntoDeInteres nuevoPdi = puntoDeInteresService.saveFromDto(pdiDTO, idCentro);
         // 2. Devuelve la respuesta 201 Created
-        return new ResponseEntity<>(pdiGuardado, HttpStatus.CREATED);
+        return new ResponseEntity<>(nuevoPdi, HttpStatus.CREATED);
+    }
+
+    // Endpoint para llenar desplegable de Centros
+    @GetMapping("/centros/{idCentro}")
+    @PreAuthorize("hasRole('ADMINISTRADOR') or hasRole('PROFESOR')")
+    public ResponseEntity<List<PuntoDeInteres>> getPdisByCentro(@PathVariable Integer idCentro) {
+        List<PuntoDeInteres> pdis = puntoDeInteresService.findByCentroId(idCentro);
+
+        if (pdis.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(pdis);
     }
 }
