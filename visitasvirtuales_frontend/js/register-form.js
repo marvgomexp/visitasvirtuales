c17fba3e3d156f1252b0f3df8ecabb70b04520b5
marvg <mge0018@alu.medac.es>
2025-11-20 12:21:04 +0100

    Actualización de formulario de registro
    Se eliminó la opción de registrarse como alumno o usuario para unificarlo todo en un solo formulario. También queda implementado el login en algunas páginas en las que faltaba

--- visitasvirtuales_frontend/js/register-form.js
+++ visitasvirtuales_frontend/js/register-form.js
@@ -1,30 +1,4 @@
-// OPCIONES: ALUMNO O DOCENTE: 
-
-// Elementos necesarios
-
-  const btnAlumno = document.getElementById('btnAlumno');
-  const btnDocente = document.getElementById('btnDocente');
-  const camposAlumno = document.getElementById('camposAlumno');
-  const camposDocente = document.getElementById('camposDocente');
-
-// Al hacer clic en el botón Alumno
-    btnAlumno.addEventListener('click', () => {
-    btnAlumno.classList.add('activo');
-    btnDocente.classList.remove('activo');
-    camposAlumno.style.display = 'block';
-    camposDocente.style.display = 'none';
-  });
-
-// Al hacer clic en el botón Docente
-    btnDocente.addEventListener('click', () => {
-    btnDocente.classList.add('activo');
-    btnAlumno.classList.remove('activo');
-    camposAlumno.style.display = 'none';
-    camposDocente.style.display = 'block';
-  });
-
-
-  // MOSTRAR CENTROS SEGÚN LA COMUNIDAD QUE SE SELECCIONE:
+ // MOSTRAR CENTROS SEGÚN LA COMUNIDAD QUE SE SELECCIONE:
 
   // Objeto con comunidades y sus centros
 
